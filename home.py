a5f6f3b6b703ef4f55843790ab226c7f5870e2ad
Scott Jenkins <sjenkins097@gmail.com>
2024-05-01 17:26:45 +0100

    update

--- home.py
+++ home.py
@@ -9,18 +9,16 @@ import numpy as np
 
 meal_plan_conn = st.connection('meal_plan_db', type='sql',autocommit=True)
 
-DATABASE_URL = 'sqlite:///meal_plan.db'  # Replace with your actual connection string
-engine = create_engine(DATABASE_URL)
+# DATABASE_URL = 'sqlite:///meal_plan.db'  # Replace with your actual connection string
+# engine = create_engine(DATABASE_URL)
 
 # Unable to delete rows from the database
 # Also want to view the tables in the database.
-
 
 # Load Meal List from Spreadsheet
 meal_df = pd.read_excel('meals.xlsx',sheet_name='meal_df')
 # Generate meal graph
 G = generate_meal_graph(meal_df=meal_df)
-
 
 with st.form('Meal Planner Form'):
     st.write("Select Date Range for Meals")
@@ -36,6 +34,7 @@ with st.form('Meal Planner Form'):
     # Form submission button
     submitted = st.form_submit_button("Generate Meal Plan")
 
+
 def insert_meals(meal_plan_conn, date_list, meal_names):
     # Get current timestamp to insert into db table
     time_now = dt.datetime.now()
@@ -43,9 +42,9 @@ def insert_meals(meal_plan_conn, date_list, meal_names):
         with meal_plan_conn.session as s:
             s.execute(text('CREATE TABLE IF NOT EXISTS meal_plan (dt_created DATETIME, date TEXT, meal TEXT);'))
             meals = dict(zip(date_list, meal_names))
-            st.write(meals)
+            # st.write(meals)
         for k in meals:
-            st.write(k, meals[k])
+            # st.write(k, meals[k])
             s.execute(text(
                 f'INSERT INTO meal_plan (dt_created, date, meal) VALUES (:dt_created, :date, :meal);'),
                 params=dict(dt_created = time_now, date=k, meal=meals[k])
@@ -60,7 +59,7 @@ if submitted:
     meal_names = generate_meal_plan(meal_df, G, date_list)
     insert_meals(meal_plan_conn, date_list, meal_names)
 
-    st.cache_data.clear()
+    # st.cache_data.clear()
     db_meals = meal_plan_conn.query('select * from meal_plan')
     st.write('planned_meals table')
     st.dataframe(db_meals)
